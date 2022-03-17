777336a4f7dc93db9a923f091ab8ab568e221f21
Griffin <gmd298@gmail.com>
2022-03-17 19:32:47 -0400

    commit tree 095923e3e05a339c068f1b1973e2a121a213426e

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// const reducer = (accumulator, batteryBatches) {
+    const totalBatteries = batteryBatches.reduce((partialSum, battery) => partialSum + battery, 0);
+    
+    console(totalBatteries);
+// }
